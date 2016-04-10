139a58e1ecb51f024a3a3fb81a5379ac74a1a20f
giorgiochiriaco <giorgio.chiriaco@gmail.com>
2016-04-10 17:39:40 +0200

    Project Hook creates the Link Type "Required Skills" if not exists. This link type correlates one task of a generic project with multiple skills.

--- src/main/java/it/pmx/atlassian/plugins/template/PmxAddProjectHook.java
+++ src/main/java/it/pmx/atlassian/plugins/template/PmxAddProjectHook.java
@@ -1,5 +1,11 @@
 package it.pmx.atlassian.plugins.template;
 
+import java.util.Collection;
+
+import com.atlassian.jira.ComponentManager;
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.link.IssueLinkType;
+import com.atlassian.jira.issue.link.IssueLinkTypeManager;
 import com.atlassian.jira.project.template.hook.AddProjectHook;
 import com.atlassian.jira.project.template.hook.ConfigureData;
 import com.atlassian.jira.project.template.hook.ConfigureResponse;
@@ -7,6 +13,8 @@ import com.atlassian.jira.project.template.hook.ValidateData;
 import com.atlassian.jira.project.template.hook.ValidateResponse;
 
 public class PmxAddProjectHook implements AddProjectHook {
+	
+	private String LINK_TYPE_NAME = "Required Skills";
 
 	@Override
 	public ValidateResponse validate(final ValidateData validateData) {
@@ -20,8 +28,16 @@ public class PmxAddProjectHook implements AddProjectHook {
 
 	@Override
 	public ConfigureResponse configure(final ConfigureData configureData) {
+		
+		IssueLinkTypeManager issueLinkTypeManager = (IssueLinkTypeManager)
+				ComponentManager.getComponentInstanceOfType(IssueLinkTypeManager.class);
+		
+		Collection<IssueLinkType> it = issueLinkTypeManager.getIssueLinkTypesByName(LINK_TYPE_NAME);
+		if ( (it==null) || it.isEmpty() ) {
+			issueLinkTypeManager.createIssueLinkType(LINK_TYPE_NAME, "Requires", "Is required by", "require-skill");
+		}
+		
 		ConfigureResponse configureResponse = ConfigureResponse.create().setRedirect("/issues/");
-
 		return configureResponse;
 	}
 }
